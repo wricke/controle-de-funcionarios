76f017dfddc9dc727882716efe339e73a3fd0d43
Wesley Matos <wrickee@gmail.com>
2019-06-09 18:22:38 -0300

    indentar código (ops)

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -1,32 +1,32 @@
 <template>
   <q-page>
-      <q-form
-        class="q-gutter-md form-login q-mx-auto"
-        ref="login"
-        @submit="onSubmit">
-        <q-input
-          v-model="email"
-          label="E-mail"
-          lazy-rules
-          :rules="[
-            val => val && val.length > 0 || 'Por favor, digite seu e-mail.',
-            val => emailValidation(val) || 'Digite um e-mail válido.',
-          ]"/>
-        <q-input
-          v-model="password"
-          label="Senha"
-          type="password"
-          lazy-rules
-          :rules="[
-            val => val && val.length > 0 || 'Por favor, digite sua senha.',
-          ]"/>
-        <div class="flex justify-end">
-          <q-btn
-            type="submit"
-            label="Entrar"
-            color="primary"/>
-        </div>
-      </q-form>
+    <q-form
+      class="q-gutter-md form-login q-mx-auto"
+      ref="login"
+      @submit="onSubmit">
+      <q-input
+        v-model="email"
+        label="E-mail"
+        lazy-rules
+        :rules="[
+          val => val && val.length > 0 || 'Por favor, digite seu e-mail.',
+          val => emailValidation(val) || 'Digite um e-mail válido.',
+        ]"/>
+      <q-input
+        v-model="password"
+        label="Senha"
+        type="password"
+        lazy-rules
+        :rules="[
+          val => val && val.length > 0 || 'Por favor, digite sua senha.',
+        ]"/>
+      <div class="flex justify-end">
+        <q-btn
+          type="submit"
+          label="Entrar"
+          color="primary"/>
+      </div>
+    </q-form>
   </q-page>
 </template>
 
